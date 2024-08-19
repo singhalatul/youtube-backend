2ba90eb62ac4cdc3ba20b7fbb95d53443b2cd615
singhalatul <atul.aggarwal2852@gmail.com>
2024-08-19 08:17:56 +0530

    Util code

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
+//for security purpose 
+//we can also use bodyparser
 //data enters from the form element
 app.use(express.json({limit:"16kb"}))
 //data enters from url
@@ -20,3 +22,5 @@ app.use(express.static("public"))
 app.use(cookieParser());
 
 export default app;
+
+//v8
