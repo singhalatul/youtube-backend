9f5390206078f23043a1fbcfebc4f25c47cd9ec2
singhalatul <atul.aggarwal2852@gmail.com>
2024-08-25 08:53:55 +0530

    healthCheck routes

--- src/app.js
+++ src/app.js
@@ -26,10 +26,12 @@ app.use(cookieParser());
 //routes
 import userRouter from './routes/user.routes.js';
 import videoRouter from './routes/video.router.js';
+import healthCheckRouter from './routes/healthCheck.routes.js';
 
 //routes declaration
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/users/video',videoRouter)
+app.use('/api/v1/users/health',healthCheckRouter)
 
 
 
---
+++ src/controllers/healthCheck.controller.js
@@ -0,0 +1,20 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const healthCheck = asyncHandler (async(req,res)=>{
+
+    try{
+
+        return res.status(200).json(
+            new ApiResponse(200,
+            {},
+            "Everything is ok"
+            )
+        )
+    }catch(err){
+        throw new ApiError(500,err.message||"internal server error")
+    }
+})
+
+export {healthCheck}
